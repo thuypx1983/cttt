ab119145f638e3b040235cd3791f9cda3640f1b2
PHAM XUAN THUY <thuypx1983@gmail.com>
2016-10-18 07:18:15 +0700

    new version

---
+++ sites/all/themes/bootstrap_business/js/scripts.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $(document).ready(function () {
+        $('.view-product-hot .views-row').matchHeight({
+            byRow: true,
+            property: 'height',
+            target: null,
+            remove: false
+        });
+    })
+})(jQuery)
